d021403744556bbcaf6654ec27e8ad98b75a9c74
Nico Guerin <guerinnico08@gmail.com>
2023-06-21 19:54:35 +0200

    enclosed navbar links in <li> tags

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,24 @@ export default function Navbar() {
     return (
         <nav className="w-full">
             <ul className="w-full flex justify-evenly font-medium text-xl border-b-2 border-slate-400">
-                <CustomLink
-                    to="/"
-                    text="All"
-                />
-                <CustomLink
-                    to="/active"
-                    text="Active"
-                />
-                <CustomLink
-                    to="/completed"
-                    text="Completed"
-                />
+                <li>
+                    <CustomLink
+                        to="/"
+                        text="All"
+                    />
+                </li>
+                <li>
+                    <CustomLink
+                        to="/active"
+                        text="Active"
+                    />
+                </li>
+                <li>
+                    <CustomLink
+                        to="/completed"
+                        text="Completed"
+                    />
+                </li>
             </ul>
             <NewTodoForm />
         </nav>
